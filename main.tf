4cfe210f2214d75705faf552b5794d228a2b6b30
ibtahaj <ibtahajgrt@gmail.com>
2024-12-11 15:50:37 +0500

    update yaml file and terraform file

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "google" {
-  project = "genuine-habitat-439408-a1"
+  project = "pure-girder-440304-b0"
   region  = "us-central1"
 }
 
 resource "google_cloudfunctions_function" "visitor_counter" {
-  name        = "muqadas-2nd"
+  name        = "ibtahaj-2nd"
   runtime     = "python311"
   entry_point = "visitor_counter"
   source_archive_bucket = "ibtahajdevops.site"
